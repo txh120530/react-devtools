fca3d6b859adc02c9aa37d069e5e6f72c238a5a5
Travis Harvey <tharvey@traviss-mbp.tnt.local>
2019-11-20 14:32:38 -0600

    Updates

---
+++ precache-manifest.91b6bee015b63d3aa42d62f692ec11c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b870da8eee72e1729f3fc600c8a6d5",
+    "url": "/react-devtools/index.html"
+  },
+  {
+    "revision": "32005ec03939e4261395",
+    "url": "/react-devtools/static/css/2.96479aa2.chunk.css"
+  },
+  {
+    "revision": "9071d2a6c3516e73c82b",
+    "url": "/react-devtools/static/css/main.07c0580b.chunk.css"
+  },
+  {
+    "revision": "32005ec03939e4261395",
+    "url": "/react-devtools/static/js/2.27158c44.chunk.js"
+  },
+  {
+    "revision": "9071d2a6c3516e73c82b",
+    "url": "/react-devtools/static/js/main.b218e5b0.chunk.js"
+  },
+  {
+    "revision": "62495d2d567b2b4dfc33",
+    "url": "/react-devtools/static/js/runtime-main.94f778de.js"
+  }
+]);
